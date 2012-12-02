43bf415474a8327a66fae180581d09e1de253e84
Lost974 <lost974.kevin@gmail.com>
2012-12-02 18:13:15 +0100

    Ajout du debut de la fonction de reconnaisance pour tronquage

--- neuron.ml
+++ neuron.ml
@@ -1,3 +1,28 @@
 class neuron
 
 end
+
+let truncate matrix = 
+  let (x,y) = (Array.length matrix ,Array.length matrix.(0))
+  and sum = ref 0
+  and i = ref 0 
+  and j = ref 0 in
+  let rec trunc mat bi_x bs_x bi_y bs_y =
+    i := bi_x;
+    sum := 0;
+    while !i < bs_x && !sum = 0 do
+      j := bi_y;
+      while !j < bs_y do
+	sum := !sum + mat.(!i).(!j);
+	j:= !j+1
+      done;
+      i := !i + 1
+    done;
+    (!i-1)
+  in trunc matrix 0 y 0 x
+
+
+let test =
+  let mat = Array.make_matrix 2 3 0 in
+  mat.(1).(2) <- 1;
+  truncate mat
